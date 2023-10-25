01707997c08434c53a8e519d8903aa17a897c2d0
Daishi00 <hasiuk9@gmail.com>
2023-10-25 11:05:46 +0200

    feat: connect server to planetscale

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -3,9 +3,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TrpcModule } from './trpc/trpc.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TrpcModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: 3306,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+      synchronize: true,
+      ssl: {
+        rejectUnauthorized: true,
+      },
+    }),
+    TrpcModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
